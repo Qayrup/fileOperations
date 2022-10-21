015080558dfdd3c71a2c60586b63c0eb95c6cb1b
Qayrup <qayrup@aliyun.com>
2022-10-21 09:09:01 +0800

    add fileDeletion function

--- fileDeletion/fileDeletion.js
+++ fileDeletion/fileDeletion.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 const path = require('path');
-// const targets = ['._', 'IT教程吧-www.it'];
 
 
 /** 
---
+++ index.js
@@ -0,0 +1,9 @@
+// 文件新增
+
+// 文件修改
+// 文件删除
+const fileDeletion = require('./fileDeletion/fileDeletion')
+
+module.exports = {
+    fileDeletion
+}
